be86e482ed2b545af401da59ab431a2515a313f9
falke_34 <falke_34@080676fd-0f56-412f-822c-f8f0d7cea3b7>
2015-03-27 13:13:16 +0000

    Removed 4.7 PS, back to last Version

--- Tools/PacketSamurai/src/com/aionemu/packetsamurai/crypt/AionGameCrypter.java
+++ Tools/PacketSamurai/src/com/aionemu/packetsamurai/crypt/AionGameCrypter.java
@@ -15,78 +15,85 @@
  *  If not, see <http://www.gnu.org/licenses/>.
  */
 
+
 package com.aionemu.packetsamurai.crypt;
 
-import com.aionemu.packetsamurai.IUserInterface;
-import com.aionemu.packetsamurai.PacketSamurai;
-import com.aionemu.packetsamurai.parser.PartType;
-import com.aionemu.packetsamurai.parser.datatree.DataTreeNodeContainer;
-import com.aionemu.packetsamurai.parser.datatree.IntValuePart;
-import com.aionemu.packetsamurai.protocol.Protocol;
-import com.aionemu.packetsamurai.protocol.protocoltree.PacketFamilly;
-import com.aionemu.packetsamurai.protocol.protocoltree.PacketFamilly.packetDirection;
-import com.aionemu.packetsamurai.session.DataPacket;
 import java.util.Arrays;
 
-public abstract class AionGameCrypter implements ProtocolCrypter {
+import com.aionemu.packetsamurai.PacketSamurai;
+import com.aionemu.packetsamurai.parser.PartType;
+import com.aionemu.packetsamurai.parser.datatree.IntValuePart;
+import com.aionemu.packetsamurai.protocol.Protocol;
+import com.aionemu.packetsamurai.protocol.protocoltree.PacketFamilly.packetDirection;
+import com.aionemu.packetsamurai.session.DataPacket;
 
-    private Protocol protocol;
-    private PartType opcodecType;
+/**
+ * @author -Nemesiss-
+ *
+ */
+public abstract class AionGameCrypter implements ProtocolCrypter
+{
+	private Protocol protocol;
+	private PartType opcodecType;
     byte[] staticKey = "nKO/WctQ0AVLbpzfBkS6NevDYT8ourG5CRlmdjyJ72aswx4EPq1UgZhFMXH?3iI9".getBytes();
     byte[] clientPacketkey;
     byte[] serverPacketkey;
-  
-    public boolean decrypt(byte[] raw, PacketFamilly.packetDirection dir) {
-        if (dir == PacketFamilly.packetDirection.clientPacket) {
-            if (clientPacketkey == null) {
+
+    public boolean decrypt(byte[] raw, packetDirection dir)
+    {
+		if(dir == packetDirection.clientPacket)
+        {
+            if (clientPacketkey == null)
                 return false;
-            }
             decode(raw, clientPacketkey);
         }
-        else {
-            if (clientPacketkey == null) {
-                if (!validatePacket(raw, getStaticClientPacketCode())) {
-                PacketSamurai.getUserInterface().log("Invalid Static Client Packet Code!!!");
+        else
+        {
+			if (clientPacketkey == null)
+            {
+				if(!validatePacket(raw, getStaticClientPacketCode()))
+                	PacketSamurai.getUserInterface().log("Invalid Static Client Packet Code!!!");
+                decodeOpcodec(raw);
+                return searchKey(Arrays.copyOf(raw, raw.length), dir);
             }
+            decode(raw, serverPacketkey);
+            if(!validatePacket(raw, getStaticServerPacketCode()))
+            	PacketSamurai.getUserInterface().log("Invalid Static Server Packet Code !!!");
             decodeOpcodec(raw);
-            return searchKey(Arrays.copyOf(raw, raw.length), dir);
-        }
-        decode(raw, serverPacketkey);
-        if (!validatePacket(raw, getStaticServerPacketCode())) {
-            PacketSamurai.getUserInterface().log("Invalid Static Server Packet Code !!!");
-        }
-        decodeOpcodec(raw);
         }
         return true;
     }
-  
-    private boolean searchKey(byte[] raw, PacketFamilly.packetDirection dir) {
-        DataPacket packet = new DataPacket(0, raw, dir, 0L, protocol);
-        if ((dir == PacketFamilly.packetDirection.serverPacket) && ("SM_KEY".equals(packet.getName()))) {
-            IntValuePart part = (IntValuePart)packet.getRootNode().getPartByName("key");
-            if (part == null) {
-                PacketSamurai.getUserInterface().log("Check your protocol there is no part called 'key' which is required in packet 0x49 SM_KEY of the GS protocol.");
+ 
+    private boolean searchKey(byte[] raw, packetDirection dir)
+    {
+        DataPacket packet = new DataPacket(0, raw, dir, 0, protocol);
+        
+        if (dir ==  packetDirection.serverPacket && "SM_KEY".equals(packet.getName()))
+        {
+            int key;
+            IntValuePart part = (IntValuePart) packet.getRootNode().getPartByName("key");
+            if(part == null)
+            {
+            	PacketSamurai.getUserInterface().log("Check your protocol there is no part called 'key' which is required in packet 0x49 SM_KEY of the GS protocol.");
                 return false;
             }
-            int key = part.getIntValue();
-            if ((this instanceof AionGame4_7_0_0Crypter)) {
-                key = key - 0x3FF2CCCF ^ 0xCD92E4DD;
-            } else if ((this instanceof AionGame4_5_0_15Crypter)) {
-                key = key - 0x3FF2CCCF ^ 0xCD92E4DD;
-            } else if ((this instanceof AionGame4_5_0_0Crypter)) {
-                key = key - 0x3FF2CCCF ^ 0xCD92E4DB;
-            } else {
-                key = key - 0x3FF2CCCF ^ 0xCD92E4D9;
-            }
+            key = part.getIntValue();
+			if (this instanceof AionGame4_3_0_0Crypter || this instanceof AionGame4_5_0_0Crypter) {
+				key = (key - 0x3FF2CCCF) ^ 0xCD92E4DB;
+			}
+			else {
+				key = (key - 0x3FF2CCCF) ^ 0xCD92E4DD;
+			}
+
             clientPacketkey = new byte[8];
-            clientPacketkey[0] = ((byte)(key & 0xFF));
-            clientPacketkey[1] = ((byte)(key >> 8 & 0xFF));
-            clientPacketkey[2] = ((byte)(key >> 16 & 0xFF));
-            clientPacketkey[3] = ((byte)(key >> 24 & 0xFF));
-            clientPacketkey[4] = -95;
-            clientPacketkey[5] = 108;
-            clientPacketkey[6] = 84;
-            clientPacketkey[7] = -121;
+            clientPacketkey[0] = (byte) (key & 0xff);
+            clientPacketkey[1] = (byte) ((key >> 8) & 0xff);
+            clientPacketkey[2] = (byte) ((key >> 16) & 0xff);
+            clientPacketkey[3] = (byte) ((key >> 24) & 0xff);
+            clientPacketkey[4] = (byte) 0xa1;
+            clientPacketkey[5] = (byte) 0x6c;
+            clientPacketkey[6] = (byte) 0x54;
+            clientPacketkey[7] = (byte) 0x87;
             serverPacketkey = new byte[8];
             System.arraycopy(clientPacketkey, 0, serverPacketkey, 0, 8);
             return true;
@@ -94,65 +101,71 @@ public abstract class AionGameCrypter implements ProtocolCrypter {
         PacketSamurai.getUserInterface().log("No key found...");
         return false;
     }
-  
-    public void decode(byte[] raw, byte[] key) {
-        int prev = raw[0]; int 
-    
-        tmp6_5 = 0; byte[] tmp6_4 = raw;tmp6_4[tmp6_5] = ((byte)(tmp6_4[tmp6_5] ^ key[0] & 0xFF));
-        for (int i = 1; i < raw.length; i++) {
-            int curr = raw[i] & 0xFF;
-            raw[i] = ((byte)(curr ^ staticKey[(i & 0x3F)] & 0xFF ^ key[(i & 0x7)] & 0xFF ^ prev));
+
+    public void decode(byte[] raw, byte[] key)
+    {
+        int prev = raw[0];
+        /** Decode first byte */
+        raw[0] ^= (key[0] &0xff);
+
+        for (int i=1; i < raw.length; i++)
+        {
+            int curr = raw[i] &0xff;
+            raw[i] = (byte)(curr ^ (staticKey[i&63] &0xff) ^ (key[i&7] &0xff) ^ prev);
             prev = curr;
         }
+
         long old = 
-            (key[0] & 0xFF) << 0 | 
-            (key[1] & 0xFF) << 8 | 
-            (key[2] & 0xFF) << 16 | 
-            (key[3] & 0xFF) << 24 | 
-            (key[4] & 0xFF) << 32 | 
-            (key[5] & 0xFF) << 40 | 
-            (key[6] & 0xFF) << 48 | 
-            (key[7] & 0xFF) << 56;
-    
+            (((long) key[0] & 0xff)) |
+            (((long)key[1] &0xff) <<  8) |
+            (((long)key[2] &0xff) << 16) |
+            (((long)key[3] &0xff) << 24) |
+            (((long)key[4] &0xff) << 32) |
+            (((long)key[5] &0xff) << 40) |
+            (((long)key[6] &0xff) << 48) |
+            (((long)key[7] &0xff) << 56);
+
         old += raw.length;
-    
-        key[0] = ((byte)(int)(old >> 0 & 0xFF));
-        key[1] = ((byte)(int)(old >> 8 & 0xFF));
-        key[2] = ((byte)(int)(old >> 16 & 0xFF));
-        key[3] = ((byte)(int)(old >> 24 & 0xFF));
-        key[4] = ((byte)(int)(old >> 32 & 0xFF));
-        key[5] = ((byte)(int)(old >> 40 & 0xFF));
-        key[6] = ((byte)(int)(old >> 48 & 0xFF));
-        key[7] = ((byte)(int)(old >> 56 & 0xFF));
+
+        key[0] = (byte)(old &0xff);
+        key[1] = (byte)(old >>  8 &0xff);
+        key[2] = (byte)(old >> 16 &0xff);
+        key[3] = (byte)(old >> 24 &0xff);
+        key[4] = (byte)(old >> 32 &0xff);
+        key[5] = (byte)(old >> 40 &0xff);
+        key[6] = (byte)(old >> 48 &0xff);
+        key[7] = (byte)(old >> 56 &0xff);
     }
-  
-    private final boolean validatePacket(byte[] raw, byte code) {
-        int opSize = opcodecType.getTypeByteNumber();
-        for (int i = 0; i < opSize; i++) {
-            if (raw[i] != (raw[(i + opSize + 1)] ^ 0xFFFFFFFF)) {
-                return false;
-            }
-        }
-        return raw[opSize] == code;
+
+	private final boolean validatePacket(byte[] raw, byte code) {
+		int opSize = opcodecType.getTypeByteNumber();
+		for (int i = 0; i < opSize; i++) {
+			if (raw[i] != ~raw[i + opSize + 1]) {
+				return false;
+			}
+		}
+		return raw[opSize] == code;
+	}
+
+    private final void decodeOpcodec(byte[] raw)
+    {
+    	int op = 0;
+    	for(int i = 0; i < opcodecType.getTypeByteNumber(); i++)
+    		op |= (raw[i] & 0xff) << (i*8);
+
+    	int decOp = decodeOpcodec(op);
+
+    	for(int i = 0; i < opcodecType.getTypeByteNumber(); i++)
+    		raw[i] = (byte) (decOp >> (i*8));
     }
-  
-    private final void decodeOpcodec(byte[] raw) {
-        int op = 0;
-        for (int i = 0; i < opcodecType.getTypeByteNumber(); i++) {
-            op |= (raw[i] & 0xFF) << i * 8;
-        }
-        int decOp = decodeOpcodec(op);
-        for (int i = 0; i < opcodecType.getTypeByteNumber(); i++) {
-            raw[i] = ((byte)(decOp >> i * 8));
-        }
-    }
-  
-    public void setProtocol(Protocol protocol) {
-        this.protocol = protocol;
-        opcodecType = protocol.getServerPacketsFamilly().getSwitchType();
-    }
-  
-    abstract int decodeOpcodec(int paramInt); 
-    abstract byte getStaticServerPacketCode(); 
-    abstract byte getStaticClientPacketCode();
+
+	public void setProtocol(Protocol protocol)
+	{
+		this.protocol = protocol;
+		this.opcodecType = protocol.getServerPacketsFamilly().getSwitchType();
+	}
+
+	abstract int decodeOpcodec(int op);
+	abstract byte getStaticServerPacketCode();
+	abstract byte getStaticClientPacketCode();
 }
